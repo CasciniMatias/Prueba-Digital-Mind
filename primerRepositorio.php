f605a046b9ac7d7a848450ebc10a7712d89d4983
Matias Cascini <matiascascini87@gmail.com>
2023-03-06 14:26:24 -0300

    Primer commit

---
+++ primerRepositorio.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+
+Con el comando git add *.js lo que hace git es agregar todos los 
+archivos que tengan extension js solamente.
+
+Cuando queremos agregar todos los archivos sin importar cual extension
+es, simplemente hacemos git add. y listo.
+
+También podemos agregar de a uno. Eso sería haciendo
+git add nombreDelArchivo.php
+
+Para ver el estado de nuestros archivos y saber si estan se hace
+git status 
+Ahi nos dice cuales hay y cuales no
+
+Para agregar usuario y mail hacemos:
+git config user.name "Nombre Apellido"
+git config user.email "nuestro@email.com"
+
+Para agregar un commit se hace:
+git commit -m "Mensaje Aclarando Que Se Cambio"
+
+Para ver el historial (de commits) de camios hacemos:
+git log
+
+
+*/
+
+?>
+
